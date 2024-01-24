7f5ebe5bc5e47d021491640d08e41ac9a3a533d1
Yogi <115236926+yogitasrivastava@users.noreply.github.com>
2024-01-24 13:42:26 +1100

    Update main.tf

--- src/az-aml-managed-vnet/deploy/Terraform/main.tf
+++ src/az-aml-managed-vnet/deploy/Terraform/main.tf
@@ -1,1 +1,31 @@
+data "azurerm_client_config" "current" {}
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    azapi = {
+      source = "Azure/azapi"
+      version = "1.11.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azapi" {
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "random_integer" "suffix" {
+  min = 10000000
+  max = 99999999
+}
