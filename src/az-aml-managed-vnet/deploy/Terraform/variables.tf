a07cee3665a7a2c68adbdd54f511c50ae8c93fcd
Yogi <115236926+yogitasrivastava@users.noreply.github.com>
2024-02-15 09:21:05 +1100

    Update variables.tf
    tested variables.tf

--- src/az-aml-managed-vnet/deploy/Terraform/variables.tf
+++ src/az-aml-managed-vnet/deploy/Terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "environment" {
   type        = string
   description = "Name of the environment"
-  default     = "nonprod"
+  default     = "poc"
 }
 
 variable "location" {
   type        = string
   description = "Location of the resources"
-  default     = "australiaeast"
+  default     = "centralindia"
 }
 
 variable "prefix" {
   type        = string
   description = "Prefix of the resource name"
-  default     = "aml"
+  default     = "mvnetaml"
 }
 
 variable "image_build_compute_name" {
   type        = string
   description = "Name of the compute cluster to be created and set to build docker images"
-  default     = "image-builder"
+  default     = "imgbuilder"
 }
